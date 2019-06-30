38b12dfb8291c9b2fbbcd0b56a2cb99e944aab3f
Freddie Pike <Freddie@colabsoftware.com>
2019-06-30 12:55:13 -0230

    Added checkers which position themselves based off the CheckerList component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import React from 'react';
 import './App.css';
 import CheckerBoard from './components/CheckerBoard';
 import CheckerBoardPattern from './components/CheckerBoardPattern';
+import CheckerList from './components/CheckerList';
 
 function App() {
   return (
     <div className="main-container">
       <CheckerBoard>
         <CheckerBoardPattern />
+        <CheckerList color="red" coordinates={
+          [
+            {
+              "x": 1, "y": 1
+            },
+            {
+              "x": 5, "y": 7
+            }
+          ]
+        } />
+        <CheckerList color="black" coordinates={
+          [
+            {
+              "x": 2, "y": 2
+            },
+            {
+              "x": 6, "y": 6
+            }
+          ]
+        } />
       </CheckerBoard>
     </div>
   );
--- src/components/CheckerBoardPattern.jsx
+++ src/components/CheckerBoardPattern.jsx
@@ -17,7 +17,6 @@ function CheckerBoardPattern() {
       }
     }
     return list;
-
   }
 
   return (
