cbe31fd117a054d3dc3b9529af4745743889a5e0
Freddie Pike <Freddie@colabsoftware.com>
2019-08-04 10:54:36 -0230

    Rust CheckerGameState workings. Basic stuct working and figured out a way to have arrays with wasm_bindgen.

--- without-a-bundler/src/lib.rs
+++ without-a-bundler/src/lib.rs
@@ -1,4 +1,8 @@
+use core::panic::PanicInfo;
 use wasm_bindgen::prelude::*;
+
+use core::slice::from_raw_parts_mut;
+use core::slice::from_raw_parts;
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
@@ -31,3 +35,112 @@ pub fn sub(a: u32, b: u32) -> u32 {
 pub fn multiply(a: u32, b: u32) -> u32 {
     a * b
 }
+
+#[wasm_bindgen]
+pub struct StreamingStats {
+  pub count: u32, 
+  sum: f64,
+}
+
+#[wasm_bindgen]
+impl StreamingStats {
+  pub fn add(&mut self, sample: f64) {
+    self.count += 1;
+    self.sum += sample;
+  }
+
+  pub fn mean(&self) -> f64 {
+    self.sum / (self.count as f64)
+  }
+
+  pub fn get_count(&self) -> u32 {
+    self.count
+  }
+
+  #[wasm_bindgen(constructor)]
+  pub fn new(count: f64) -> StreamingStats {
+    StreamingStats {
+      count: 0,
+      sum: 0.0,
+    }
+  }
+}
+
+
+#[wasm_bindgen]
+pub struct CheckerGameState {
+  red_checker_list_offset: *const u8,
+  red_checker_list_size: usize, 
+  black_checker_list_offset: *const u8, 
+  black_checker_list_size: usize, 
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn console_log_str(s: &str);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn console_log_u8(n: u8);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn console_log_many(a: u32, b: &JsValue);
+}
+
+
+#[wasm_bindgen]
+impl CheckerGameState {
+  pub fn red_checker_list_offset(&self) -> *const u8 {
+      self.red_checker_list_offset
+  }
+
+  pub fn red_checker_list_size(&self) -> usize {
+      self.red_checker_list_size
+  }
+
+  pub fn black_checker_list_offset(&self) -> *const u8 {
+      self.black_checker_list_offset
+  }
+
+  pub fn black_checker_list_size(&self) -> usize {
+      self.black_checker_list_size
+  }
+
+  pub fn testy_test(&self) {
+    // let src_img: &[u32];
+    let red_checker_list_workable: &mut [u8];
+    unsafe { 
+      red_checker_list_workable = from_raw_parts_mut::<u8>(
+        self.red_checker_list_offset as *mut u8, self.red_checker_list_size
+      );
+    }
+    console_log_str("abc.");
+    for i in 0..self.red_checker_list_size { 
+      console_log_str("red_checker_list_workable before assignment.");
+      console_log_u8(red_checker_list_workable[0]);
+      red_checker_list_workable[i] = 55;
+      console_log_str("red_checker_list_workable after assignment.");
+      console_log_u8(red_checker_list_workable[0]);
+    }
+  }
+
+
+  #[wasm_bindgen(constructor)]
+  pub fn new(red_checker_list_bytes: &[u8], black_checker_list_bytes: &[u8]) -> CheckerGameState {
+    CheckerGameState {
+      red_checker_list_offset: red_checker_list_bytes.as_ptr(),
+      red_checker_list_size: red_checker_list_bytes.len(),
+      black_checker_list_offset: black_checker_list_bytes.as_ptr(),
+      black_checker_list_size: black_checker_list_bytes.len(),
+    }
+  }
+}
+
+#[wasm_bindgen]
+pub fn renderCheckerGameState() -> CheckerGameState {
+  let mut texture1 = vec![1, 2, 3];
+  // texture1.push(1);
+  let texture2 = Vec::new();
+  CheckerGameState::new(&texture1, &texture2)
+}
+
